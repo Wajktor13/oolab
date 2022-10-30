8a5d3647a4655d62da4cce5bb5b56014c47470d8
Wajktor13 <wiktorwilkusz13@gmail.com>
2022-10-30 19:56:39 +0100

    clean

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -6,22 +6,12 @@ import static java.lang.System.out;
 
 
 public class World {
+
+    //        ./gradlew.bat run --args="f l x backward e r"
+
     public static void main(String[] args){
-//        ./gradlew.bat run --args="f l x backward e r"
 
         out.println("system has started\n");
-
-        Animal newAnimal = new Animal();
-        OptionParser op = new OptionParser();
-
-        MoveDirection[] commands = op.parse(args);
-
-        for (MoveDirection command : commands){
-            newAnimal.move(command);
-        }
-
-        out.println(Arrays.toString(commands));
-        out.println(newAnimal);
 
         out.println("\nsystem has stopped");
     }
