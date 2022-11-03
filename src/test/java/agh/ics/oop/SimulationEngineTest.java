d77f62deeb4734145af5a938a97ee377c63620b0
Wajktor13 <wiktorwilkusz13@gmail.com>
2022-11-03 16:14:28 +0100

    lab4 - corrected v2

--- src/test/java/agh/ics/oop/SimulationEngineTest.java
+++ src/test/java/agh/ics/oop/SimulationEngineTest.java
@@ -25,7 +25,7 @@ public class SimulationEngineTest {
     }
 
     @Test
-    void movingAnimals(){
+    void movingAnimals1(){
         MoveDirection[] moves = {MoveDirection.RIGHT, MoveDirection.BACKWARD, MoveDirection.FORWARD};
         Vector2d[] initialPositions = {new Vector2d(1, 2), new Vector2d(2, 1), new Vector2d(1, 0)};
         RectangularMap map = new RectangularMap(3, 3);
@@ -42,5 +42,33 @@ public class SimulationEngineTest {
         assertFalse(map.isOccupied(new Vector2d(1, 0)));
         assertFalse(map.isOccupied(new Vector2d(2, 1)));
         assertFalse(map.isOccupied(new Vector2d(2, 2)));
+    }
+
+    @Test
+    void movingAnimals2(){
+        OptionsParser op = new OptionsParser();
+        MoveDirection[] moves = op.parse(new String[]{"f", "b", "r", "l", "f", "x", "f", "r", "r", "f", "f", "f",
+                                                      "f", "f", "f","y", "f", "f"});
+        Vector2d[] initialPositions = {new Vector2d(2, 2), new Vector2d(3, 4)};
+        RectangularMap map = new RectangularMap(10, 5);
+        SimulationEngine engine = new SimulationEngine(moves, map, initialPositions);
+
+        engine.run();
+
+        assertTrue(map.isOccupied(new Vector2d(2, 0)));
+        assertTrue(map.isOccupied(new Vector2d(3, 4)));
+        assertEquals(((Animal)map.objectAt(new Vector2d(2, 0))).getDirection(), MapDirection.SOUTH);
+        assertEquals(((Animal)map.objectAt(new Vector2d(3, 4))).getDirection(), MapDirection.NORTH);
+
+        for (int x = 0; x < 10; x++){
+            for (int y = 0; y < 5; y++){
+                if ((x == 2 && y ==0) || (x == 3 && y ==4)){
+                    continue;
+                }
+                else {
+                    assertFalse(map.isOccupied(new Vector2d(x, y)));
+                }
+            }
+        }
     }
 }
